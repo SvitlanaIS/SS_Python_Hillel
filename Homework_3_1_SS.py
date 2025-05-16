2e83dbfc60b4ff639a9598f0db134c596870a565
SvitlanaIS <svetlana.stakhorska@gmail.com>
2025-05-17 01:06:48 +0300

    Add files via upload

---
+++ Homework_3_1_SS.py
@@ -0,0 +1,21 @@
+number1 = int(input ("Please enter a first number from the keyboard: "))
+number2 = int(input ("Please enter a second number from the keyboard: "))
+operation = str(input ("Please enter a mathematical operation (+, -, *, /):"))
+match operation:
+     case "+":
+        result = number1 + number2
+        print(result)
+     case "-":
+         result = number1 - number2
+         print(result)
+     case "*":
+         result = number1 * number2
+         print(result)
+     case "/":
+         if number2 != 0:
+             result = number1 / number2
+             print(result)
+         else:
+             print("Division by 0 is impossible")
+     case _:
+         print("Invalid operator entered")
